32f05fc68be4dd212e28eb004d1abcf0d7ada39a
Gaël Lhez <glhez@users.noreply.github.com>
2020-09-09 00:05:54 +0200

    add Awaitibility

--- com.github.glhez.eclipse.plugins.staticimport/src/main/java/com/github/glhez/eclipse/plugins/staticimport/PreferenceInitializer.java
+++ com.github.glhez.eclipse.plugins.staticimport/src/main/java/com/github/glhez/eclipse/plugins/staticimport/PreferenceInitializer.java
@@ -1,10 +1,14 @@
 package com.github.glhez.eclipse.plugins.staticimport;
 
 import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.regex.Pattern;
 
 import org.eclipse.jdt.ui.PreferenceConstants;
 import org.eclipse.jface.preference.IPreferenceStore;
@@ -16,113 +20,133 @@ import org.eclipse.ui.IStartup;
  * @author gael.lhez
  */
 public class PreferenceInitializer implements IStartup {
-  private final IPreferenceStore javaPreferenceStore;
-
-  public PreferenceInitializer() {
-    this.javaPreferenceStore = PreferenceConstants.getPreferenceStore();
-  }
+  private static final String PREFERENCE_DELIMITER = ";";
 
   @Override
   public void earlyStartup() {
-    final Set<String> initial = getStaticImports();
+    final IPreferenceStore store = PreferenceConstants.getPreferenceStore();
+    final String key = PreferenceConstants.CODEASSIST_FAVORITE_STATIC_MEMBERS;
 
-    final Set<String> copy = new LinkedHashSet<>(initial);
+    final List<String> list = Pattern.compile(Pattern.quote(PREFERENCE_DELIMITER))
+                                     .splitAsStream(store.getString(key))
+                                     .map(String::trim)
+                                     .filter(s -> !s.isEmpty())
+                                     .collect(toList());
 
-    // jdk [basic types]
-    copy.add(newTypeImport("java.lang.String"));
-    copy.add(newTypeImport("java.lang.Character"));
-    copy.add(newTypeImport("java.lang.Boolean"));
-    copy.add(newTypeImport("java.lang.Byte"));
-    copy.add(newTypeImport("java.lang.Short"));
-    copy.add(newTypeImport("java.lang.Integer"));
-    copy.add(newTypeImport("java.lang.Long"));
-    copy.add(newTypeImport("java.lang.Float"));
-    copy.add(newTypeImport("java.lang.Double"));
-
-    // jdk [util]
-    copy.add(newTypeImport("java.lang.Math"));
-
-    copy.add(newTypeImport("java.util.Objects"));
-    copy.add(newTypeImport("java.util.Optional"));
-    copy.add(newTypeImport("java.util.OptionalInt"));
-    copy.add(newTypeImport("java.util.OptionalLong"));
-    copy.add(newTypeImport("java.util.OptionalDouble"));
-    copy.add(newTypeImport("java.util.regex.Pattern"));
-
-    copy.add(newTypeImport("java.util.concurrent.Executors"));
-
-    // jdk [lambda]
-    copy.add(newTypeImport("java.util.Comparator"));
-    copy.add(newTypeImport("java.util.function.Function"));
-    copy.add(newTypeImport("java.util.function.Predicate"));
-    copy.add(newTypeImport("java.util.function.BinaryOperator"));
-    copy.add(newTypeImport("java.util.function.UnaryOperator"));
-    copy.add(newTypeImport("java.util.function.DoubleUnaryOperator"));
-    copy.add(newTypeImport("java.util.function.IntUnaryOperator"));
-    copy.add(newTypeImport("java.util.function.LongBinaryOperator"));
-
-    // jdk [collection and stream]
-    copy.add(newTypeImport("java.util.EnumSet"));
-    copy.add(newTypeImport("java.util.Collections"));
-    copy.add(newTypeImport("java.util.Arrays"));
-    copy.add(newTypeImport("java.util.stream.Stream"));
-    copy.add(newTypeImport("java.util.stream.IntStream"));
-    copy.add(newTypeImport("java.util.stream.LongStream"));
-    copy.add(newTypeImport("java.util.stream.DoubleStream"));
-    copy.add(newTypeImport("java.util.stream.Collectors"));
-
-    // jdk 9++ [collection and stream]
-    copy.add(newTypeImport("java.util.List"));
-    copy.add(newTypeImport("java.util.Map"));
-
-    // jdk [nio2]
-    copy.add(newTypeImport("java.nio.file.Files"));
-    copy.add(newTypeImport("java.nio.channels.FileChannel"));
-
-    // jdk [awt/swing]
-    copy.add(newTypeImport("java.awt.Toolkit"));
-
-    copy.add(newTypeImport("javax.imageio.ImageIO"));
-
-    copy.add(newTypeImport("javax.swing.SwingUtilities"));
-    copy.add(newTypeImport("javax.swing.BorderFactory"));
-    copy.add(newTypeImport("javax.swing.UIManager"));
-
-    // Test API
-    copy.add(newTypeImport("org.mockito.Mockito"));
-    copy.add(newTypeImport("org.mockito.ArgumentMatchers"));
-
-    copy.add(newTypeImport("org.assertj.core.api.Assertions"));
-    copy.add(newTypeImport("org.assertj.core.api.AssertionsForClassTypes"));
-    copy.add(newTypeImport("org.assertj.core.api.AssertionsForInterfaceTypes"));
-
-    copy.add(newTypeImport("org.junit.jupiter.api.Assertions"));
-    copy.add(newTypeImport("org.junit.jupiter.api.Assumptions"));
-    copy.add(newTypeImport("org.junit.Assert"));
-    copy.add(newTypeImport("org.junit.Assume"));
-
-    // apache
-    copy.add(newTypeImport("org.apache.commons.lang3.StringUtils"));
-    copy.add(newTypeImport("org.apache.commons.text.StringEscapeUtils"));
-
-    if (!copy.equals(initial)) {
-      setStaticImports(copy);
+    final LinkedHashSet<String> imports = new LinkedHashSet<>();
+    imports.addAll(list);
+    if (imports.addAll(getDefaultImports())) {
+      final String value = imports.stream().collect(joining(PREFERENCE_DELIMITER));
+      store.setDefault(key, value);
     }
   }
 
-  private String newTypeImport(final String name) {
-    return name + ".*";
+  private static SortedSet<String> getDefaultImports() {
+    final ImportList copy = new ImportList();
+
+    // jdk [basic types]
+    copy.addTypeImport("java.lang.String");
+    copy.addTypeImport("java.lang.Character");
+    copy.addTypeImport("java.lang.Boolean");
+    copy.addTypeImport("java.lang.Byte");
+    copy.addTypeImport("java.lang.Short");
+    copy.addTypeImport("java.lang.Integer");
+    copy.addTypeImport("java.lang.Long");
+    copy.addTypeImport("java.lang.Float");
+    copy.addTypeImport("java.lang.Double");
+
+    // jdk [util]
+    copy.addTypeImport("java.lang.Math");
+
+    copy.addTypeImport("java.util.Objects");
+    copy.addTypeImport("java.util.Optional");
+    copy.addTypeImport("java.util.OptionalInt");
+    copy.addTypeImport("java.util.OptionalLong");
+    copy.addTypeImport("java.util.OptionalDouble");
+    copy.addTypeImport("java.util.regex.Pattern");
+
+    copy.addTypeImport("java.util.concurrent.Executors");
+
+    // jdk [lambda]
+    copy.addTypeImport("java.util.Comparator");
+    copy.addTypeImport("java.util.function.Function");
+    copy.addTypeImport("java.util.function.Predicate");
+    copy.addTypeImport("java.util.function.BinaryOperator");
+    copy.addTypeImport("java.util.function.UnaryOperator");
+    copy.addTypeImport("java.util.function.DoubleUnaryOperator");
+    copy.addTypeImport("java.util.function.IntUnaryOperator");
+    copy.addTypeImport("java.util.function.LongBinaryOperator");
+
+    // jdk [collection and stream]
+    copy.addTypeImport("java.util.EnumSet");
+    copy.addTypeImport("java.util.Collections");
+    copy.addTypeImport("java.util.Arrays");
+    copy.addTypeImport("java.util.stream.Stream");
+    copy.addTypeImport("java.util.stream.IntStream");
+    copy.addTypeImport("java.util.stream.LongStream");
+    copy.addTypeImport("java.util.stream.DoubleStream");
+    copy.addTypeImport("java.util.stream.Collectors");
+
+    // jdk 9++ [collection and stream]
+    copy.addTypeImport("java.util.List");
+    copy.addTypeImport("java.util.Map");
+
+    // time based api
+    copy.addTypeImport("java.time.Duration");
+    copy.addTypeImport("java.util.concurrent.TimeUnit");
+
+    // jdk [nio2]
+    copy.addTypeImport("java.nio.file.Files");
+    copy.addTypeImport("java.nio.channels.FileChannel");
+
+    // jdk [awt/swing]
+    copy.addTypeImport("java.awt.Toolkit");
+
+    copy.addTypeImport("javax.imageio.ImageIO");
+
+    copy.addTypeImport("javax.swing.SwingUtilities");
+    copy.addTypeImport("javax.swing.BorderFactory");
+    copy.addTypeImport("javax.swing.UIManager");
+
+    // Test API
+    copy.addTypeImport("org.mockito.Mockito");
+    copy.addTypeImport("org.mockito.ArgumentMatchers");
+
+    copy.addTypeImport("org.assertj.core.api.Assertions");
+    copy.addTypeImport("org.assertj.core.api.AssertionsForClassTypes");
+    copy.addTypeImport("org.assertj.core.api.AssertionsForInterfaceTypes");
+
+    copy.addTypeImport("org.junit.jupiter.api.Assertions");
+    copy.addTypeImport("org.junit.jupiter.api.Assumptions");
+    copy.addTypeImport("org.junit.Assert");
+    copy.addTypeImport("org.junit.Assume");
+
+    copy.addTypeImport("org.hamcrest.Matchers");
+
+    copy.addTypeImport("org.awaitility.Awaitility");
+
+    // apache
+    copy.addTypeImport("org.apache.commons.lang3.StringUtils");
+    copy.addTypeImport("org.apache.commons.text.StringEscapeUtils");
+
+    return copy.toUnmodifiableSortedSet();
   }
 
-  private Set<String> getStaticImports() {
-    return new LinkedHashSet<>(
-        Arrays.asList(javaPreferenceStore.getString(PreferenceConstants.CODEASSIST_FAVORITE_STATIC_MEMBERS)
-                                         .trim()
-                                         .split("\\s*;\\s*")));
-  }
+  static class ImportList {
+    private final TreeSet<String> imports;
 
-  private void setStaticImports(final Set<String> imports) {
-    final String value = imports.stream().collect(joining(";"));
-    javaPreferenceStore.setValue(PreferenceConstants.CODEASSIST_FAVORITE_STATIC_MEMBERS, value);
+    public ImportList() {
+      this.imports = new TreeSet<>();
+    }
+
+    public SortedSet<String> toUnmodifiableSortedSet() {
+      return Collections.unmodifiableSortedSet(new TreeSet<>(imports));
+    }
+
+    public ImportList addTypeImport(final String packageName) {
+      imports.add(packageName + ".*");
+      return this;
+    }
+
   }
 }
